e57d2f92bf8460dc39fe537f3ea2aa00bbccc19f
konigorikun <konishitech@gmail.com>
2023-04-24 17:00:33 +0900

    Postバリデーションとアソシエーションの設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
-class Post < ApplicationRecord
+class Post < ApplicationRecord  
+  belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :public
+
+  validates :place, presence: true
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :public_id, numericality: { other_than: 1, message: "can't be blank" }  
 end
