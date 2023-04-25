4841ef2c0eefb6c067746825efdc6b4925f8edd0
konigorikun <konishitech@gmail.com>
2023-04-25 20:53:27 +0900

    カテゴリー内の編集

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,8 @@
 class Category < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
-    { id: 2, name: '建物' },
-    { id: 3, name: 'ホテル' },
+    { id: 2, name: '建造物' },
+    { id: 3, name: 'ホテル,旅館' },
     { id: 4, name: 'グルメ,料理' },
     { id: 5, name: '自然' },
     { id: 6, name: '世界遺産' },
