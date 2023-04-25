8aa6f673de4e1b31879e970f8b29bf7b6c4eaf08
konigorikun <konishitech@gmail.com>
2023-04-25 22:10:45 +0900

    投稿機能コントローラー

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,28 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
   def index
+    @posts = Post.all
   end
+
+  def new
+    @post = Post.new
+  end 
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:image, :place, :content, :category_id, :public_id).merge(user_id: current_user.id)
+  end
+
 end
