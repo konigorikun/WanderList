6933760c3c083e131a9d10c696ed2c02cbbcd8d7
konigorikun <konishitech@gmail.com>
2023-05-03 22:18:48 +0900

    Post編集画面への遷移

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'tops#index'
-  resources :posts, only: [:index, :new, :create, :show]
+  resources :posts, only: [:index, :new, :create, :show, :edit]
   get '/category/:id', to: 'posts#index', as: 'category_image'
 end
